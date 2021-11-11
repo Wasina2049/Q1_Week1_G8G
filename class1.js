8efec307ac42b59b400f48354c54c9fab8da5502
Wasina <2049@nivais.com>
2021-11-11 12:23:34 +0700

    this is the message of my first commit

--- class1.js
+++ class1.js
@@ -1,9 +1,9 @@
-console.log('Hi Im Mr. Cristian from your ILT Class')
+console.log('Hi Im Wasina from your ILT Class')
 
-var name = 'Cristian', surname = 'Angulo' // how we declare/create variables
+var name = 'Wasina', surname = 'Itthipoowadol' // how we declare/create variables
 var age = 30
 
-age = '30 years'
+age = '13 years'
 
 console.log('Hello ' + name + ' ' + surname)
 console.log('I am ' + age)
